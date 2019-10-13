fb8acd7afcc1340b6b6f0a07804c39aba881f24f
coderinker <742870992@qq.com>
2019-10-13 17:44:06 +0800

    add index and bootstap

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -6,22 +6,17 @@ package life.majiang.community.controller;
  * @Date:2019/10/13-0:44
  * @Version:0.1
  */
-
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Administrator
  */
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
